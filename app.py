f23fea59643a39cc278cae16c8fdd3d0e9b09db2
Kevin Jay <kevinjay566@gmail.com>
2021-10-15 20:40:03 -0700

    new grid

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pytz
 
 app_window = tkinter.Tk()
 app_window.title("My Digital Time")
-app_window.geometry("1000x1000")
+app_window.geometry("1500x1000")
 app_window.resizable(None, None)
 
 text = 'Local Time'
@@ -15,6 +15,8 @@ background = "#f2e750"
 foreground= "#363529"
 border_width = 25
 time_live = time.strftime("%m %B %Y, %H:%M:%S")
+
+timezones = ['Australia/Sydney', 'Israel', 'US/Eastern', 'US/Central', 'US/Mountain', 'US/Pacific']
 
 
 label0 = tkinter.Label(app_window,  font=text_font, bg=background, fg=foreground, bd=border_width)
@@ -27,10 +29,13 @@ label2 = tkinter.Label(app_window, font=text_font, bg=background, fg=foreground,
 label2.grid(row=2, column=0)
 
 label3 = tkinter.Label(app_window,  font=text_font, bg=background, fg=foreground, bd=border_width)
-label3.grid(row=3, column=0)
+label3.grid(row=0, column=1)
 
 label4 = tkinter.Label(app_window,  font=text_font, bg=background, fg=foreground, bd=border_width)
-label4.grid(row=4, column=0)
+label4.grid(row=1, column=1)
+
+label5 = tkinter.Label(app_window,  font=text_font, bg=background, fg=foreground, bd=border_width)
+label5.grid(row=2, column=1)
 
 
 def digital_clock():
@@ -39,6 +44,7 @@ def digital_clock():
     label2.config(text='Chicago \n' + datetime.now(pytz.timezone('US/Central')).strftime("%b %d %Y %H:%M:%S")) # 'US/Central'
     label3.config(text='Denver \n' + datetime.now(pytz.timezone('US/Mountain')).strftime("%b %d %Y %H:%M:%S")) # 'US/Mountain'
     label4.config(text='Reno/Seattle \n' + datetime.now(pytz.timezone('US/Pacific')).strftime("%b %d %Y %H:%M:%S"))
+    label5.config(text='Tel Aviv \n' + datetime.now(pytz.timezone('Israel')).strftime("%b %d %Y %H:%M:%S"))
     label0.after(200, digital_clock)
 
 
